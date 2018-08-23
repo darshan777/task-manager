67b7a52db3c79cdb2120d2e1699146cd2b632ffc
darshan2494 <sanghavi.da@husky.neu.edu>
2018-08-23 18:01:34 -0400

    Authentication component Updated

--- task-frontend/src/app/authentication/authentication.component.ts
+++ task-frontend/src/app/authentication/authentication.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { User } from '../models/index';
+import { UserServiceService } from '../Service/user-service.service';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-authentication',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./authentication.component.css']
 })
 export class AuthenticationComponent implements OnInit {
+  users: User[];
+  user: User;
+  authenticateUser: FormGroup;
 
-  constructor() { }
+  constructor(private userService: UserServiceService) { }
 
   ngOnInit() {
+    this.authenticateUser = new FormGroup({
+      'userName': new FormControl(''),
+      'userPassword': new FormControl('')
+    });
+    this.getAllUsers();
+
+  }
+  getAllUsers() {
+    this.userService.getAllUsers().subscribe(
+      (response) => { this.users =  response;  }
+    );
+  }
+ /* onSubmit() {
+    this.users.forEach(function(user) {
+      console.log(user.userName);
+      let name = this.authenticateUser.value.userName;
+      console.log(this.authenticateUser.value.userName);
+    });
+  }*/
+
+  onSubmit() {
+  let name = this.authenticateUser.value.userName;
+  let password = this.authenticateUser.value.userPassword;
+    console.log(typeof(name));
+    this.users.forEach(function(user) {
+       if (user.userName === name) {
+          if (user.userPassword === password){
+              console.log(name);
+          }
+       }
+    });
   }
 
 }
